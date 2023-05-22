e15af4c695d272c67e3ac4972b8dbfeb7dfebc71
wonseok <dnjstjr245@gmail.com>
2023-05-23 08:26:47 +0900

    chore: buildSrc 모듈 추가

---
+++ buildSrc/src/main/java/com/konkuk/photomate/Configuration.kt
@@ -0,0 +1,13 @@
+package com.konkuk.photomate
+
+object Configuration {
+    const val packageName = "com.konkuk.photomate"
+    const val compileSdk = 33
+    const val targetSdk = 33
+    const val minSdk = 24
+    const val majorVersion = 1
+    const val minorVersion = 0
+    const val patchVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val versionCode = 1
+}
