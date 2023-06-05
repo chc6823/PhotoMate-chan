f89a613f5064d6a2a92799699dc9cf3dba769a21
dlawltn0708 <dlawotn3@naver.com>
2023-06-05 18:41:58 +0900

    fix: 리뷰 스크린 사진 첨부 칸 추가

--- app/src/main/java/com/konkuk/photomate/presentation/screens/review/ReviewScreen.kt
+++ app/src/main/java/com/konkuk/photomate/presentation/screens/review/ReviewScreen.kt
@@ -6,20 +6,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,6 +40,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.konkuk.photomate.R
 import com.willy.ratingbar.ScaleRatingBar
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -121,11 +124,40 @@ fun ReviewScreen() {
                 )
             }
             //---------
-            Spacer(modifier = Modifier.height((screenHeight * 0.03f)))
+            Spacer(modifier = Modifier.height((screenHeight * 0.01f)))
             //---------
             Divider(color = Color.Gray, modifier = Modifier.fillMaxWidth())
             //---------
-            Spacer(modifier = Modifier.height((screenHeight * 0.2f)))
+            Spacer(modifier = Modifier.height((screenHeight * 0.025f)))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(screenWidth * 0.26f)
+                        .height(screenHeight * 0.14f)
+                        .background(Color(0xFFD9D9D9), RoundedCornerShape(10.dp))
+                        .padding(12.dp)
+                ) {}
+                Spacer(modifier = Modifier.width((screenWidth * 0.02f)))
+                Box(
+                    modifier = Modifier
+                        .width(screenWidth * 0.26f)
+                        .height(screenHeight * 0.14f)
+                        .background(Color(0xFFD9D9D9), RoundedCornerShape(10.dp))
+                        .padding(12.dp)
+                ) {}
+                Spacer(modifier = Modifier.width((screenWidth * 0.02f)))
+                Box(
+                    modifier = Modifier
+                        .width(screenWidth * 0.26f)
+                        .height(screenHeight * 0.14f)
+                        .background(Color(0xFFD9D9D9), RoundedCornerShape(10.dp))
+                        .padding(12.dp)
+                ) {}
+            }
+            Spacer(modifier = Modifier.height((screenHeight * 0.015f)))
 
             TextField(
                 value = "",
